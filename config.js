6caa308fa0a0923dbc4c1361a2ccd96f87e69e79
Yehonal <yehonal.azeroth@gmail.com>
2017-06-19 17:45:55 +0200

    Update config.js

--- config.js
+++ config.js
@@ -47,20 +47,21 @@
     var realm = QueryString.realm || getCookie("realm");
 
     switch (realm) {
-        case "azerothshard":
-            document.cookie = "realm=azerothshard";
-            /* Edit with path of TC-JSON-API */
-            app.api = "http://azerothshard.org/modules/TC-JSON-API/public/index.php/";
-
-            app.serverName = "AzerothShard";
-            break;
         case "newage":
             document.cookie = "realm=newage";
             /* Edit with path of TC-JSON-API */
-            app.api = "http://server.wownewage.com/TC-JSON-API/public/index.php/";
+            app.api = location.protocol+"//server.wownewage.com/TC-JSON-API/public/index.php/";
 
             app.serverName = "NewAge";
             break;
+        case "azerothshard":
+        default:
+            document.cookie = "realm=azerothshard";
+            /* Edit with path of TC-JSON-API */
+            app.api = location.protocol+"//azerothshard.org/modules/TC-JSON-API/public/index.php/";
+
+            app.serverName = "AzerothShard";
+        break;
     }
 
     /* Edit with path of the SpellWork API e.g http://trinitycore.cloudapp.net/spells/ */
